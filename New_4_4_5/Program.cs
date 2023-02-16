a79ac204d916f38f19e241c9dea2fa37bc4d96fd
Andentu <severalmaz97@gmail.com>
2023-02-16 20:59:11 +0300

    Добавил код для получения значений от пользователя и записи их в кортедж, а также вывод полученных результатов в одну строку

--- New_4_4_5/Program.cs
+++ New_4_4_5/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            
+            (string Name, string Type, double Age, int NameCount) Pet;
+            Console.Write("Введите кличку животного: ");
+            Pet.Name = Console.ReadLine();
+            Console.Write("\nВведите тип домашнего животного: ");
+            Pet.Type = Console.ReadLine();
+            Console.Write("\nВведите возраст животного: ");
+            Pet.Age = double.Parse(Console.ReadLine());
+            Pet.NameCount = Pet.Name.Length;
+
+            Console.Write($"\nКличка: {Pet.Name}, тип: {Pet.Type}, возраст: {Pet.Age}, количество букв в кличке: {Pet.NameCount}");
+            Console.ReadKey();
         }
     }
 }
